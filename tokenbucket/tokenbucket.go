7cd1e141c193ba41b98c748e27bc4350b5d8df2b
Marc Meiners <marc@marcmeiners.com>
2024-03-18 19:56:03 +0100

    added first version of stepwell

--- tokenbucket/tokenbucket.go
+++ tokenbucket/tokenbucket.go
@@ -16,3 +16,16 @@ type TokenBucket struct {
 	// Store as Unix timestamp to be able to use atomic operations
 	lastRefill int64
 }
+
+func NewTokenBucketByType(type int, capacity uint64, refillRate float64, lastRefill time.Time){
+	switch type {
+		case 1:
+			return NewTokenBucketTrivial(capacity, refillRate, lastRefill)
+		case 2:
+			return NewTokenBucketAtomicLoops(capacity, refillRate, lastRefill)
+		case 3:
+			return NewTokenBucketLock(capacity, refillRate, lastRefill)
+		default:
+			return NewTokenBucketTrivial(capacity, refillRate, lastRefill)
+	}
+}
